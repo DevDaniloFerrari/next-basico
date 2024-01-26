35dc62fb766d3d95a18ae53824750e21117fdaa9
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-01-26 08:06:37 -0300

    creating api

---
+++ app/api/examples/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({ message: "Hello - GET" });
+}
+
+export async function POST() {
+  return NextResponse.json({ message: "Hello - POST" });
+}
+
+export async function PUT() {
+  return NextResponse.json({ message: "Hello - PUT" });
+}
+
+export async function DELETE() {
+  return NextResponse.json({ message: "Hello - DELETE" });
+}
+
+export async function PATCH() {
+  return NextResponse.json({ name: "Hello - PATCH" });
+}
